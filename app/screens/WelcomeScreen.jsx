b1d98dfe719129474a5adf217b5a17a8b38f7200
pumacens <dafflorezc@correo.udistrital.edu.co>
2020-10-07 00:26:57 -0500

    Added app folder, screens folder and migrated welcome screen as a component in screen folder

---
+++ app/screens/WelcomeScreen.jsx
@@ -0,0 +1,151 @@
+import React from 'react';
+import { Picker } from "@react-native-community/picker";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableNativeFeedback,
+  Dimensions
+} from "react-native";
+import { faSortDown, faFileImport, faBars } from "@fortawesome/free-solid-svg-icons";
+
+const { width, height } = Dimensions.get("screen");
+
+function WelcomeScreen() {
+    return (
+        <ImageBackground
+          style={styles.portada}
+          source={require("../assets/portada2-op.png")}
+        >
+          <View style={styles.hero}>
+            <FontAwesomeIcon style={styles.botonHambuergesa} size={25} icon= { faBars } />
+            <Text style={[styles.texto, styles.titulo]}>ForestKeys</Text>
+            <Text style={[styles.subTitulo]}>
+              Seleccione la clave a utilizar
+            </Text>
+            <View style={styles.dropdown}>
+              <Picker
+                mode="dropdown"
+                style={{height: 50, width: '100%', color: 'white'}}
+              >
+                <Picker.Item label="Lista de claves..." value="..." />
+                <Picker.Item label="a" value="..." />
+                <Picker.Item label="b" value="..." />
+                <Picker.Item label="c" value="..." />
+              </Picker>
+              <FontAwesomeIcon
+                style={[styles.iconDropClaves]}
+                icon={faSortDown}
+              />
+            </View>
+          </View>
+
+          <View style={styles.seccionPortadaBotones}>
+            <TouchableNativeFeedback title="IDENTIFICAR PLANTA">
+              <View style={styles.botonPortada}>
+                <Text style={styles.textoBoton}>IDENTIFICAR PLANTA</Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback title="LISTA DE ESPECIES DE LA CLAVE">
+              <View style={styles.botonPortada}>
+                <Text style={styles.textoBoton}>LISTA DE ESPECIES DE LA CLAVE</Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback title="IMPORTAR CLAVE">
+              <View style={styles.botonPortada}>
+                <FontAwesomeIcon size={20} style={styles.iconImportarClave} icon={ faFileImport } />
+                <Text style={[styles.textoBoton, {right: 7}]}>IMPORTAR CLAVE</Text>
+              </View>
+            </TouchableNativeFeedback>
+          </View>
+        </ImageBackground>
+    )
+}
+
+
+const styles = StyleSheet.create({
+  portada: {
+    width: width
+  },
+
+  hero: {
+    height: "50%",
+    justifyContent: "center",
+    backgroundColor: "#000000BB",
+    alignItems: "center",
+    paddingBottom: 15,
+  },
+
+  botonHambuergesa: {
+    color: 'white',
+    alignSelf: 'flex-end',
+    right: 10,
+    bottom: 8
+  },
+
+  titulo: {
+    fontSize: 40,
+    color: "white",
+  },
+
+  seccionPortadaBotones: {
+    height: "50%",
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: "#00000066",
+    paddingVertical: 30
+  },
+
+  subTitulo: {
+    color: "white",
+    marginBottom: 45
+  },
+
+  dropdown: {
+    borderBottomColor: "#FFF",
+    borderBottomWidth: 2,
+    marginTop: 25,
+    width: "85%",
+    flexDirection: "row",
+    borderLeftColor: '#FFFFFF33',
+    borderLeftWidth: 1,
+    borderRightColor: '#FFFFFF33',
+    borderRightWidth: 1,
+    paddingHorizontal: 7,
+  },
+
+  iconDropClaves: {
+    color: "white",
+    position: "absolute",
+    left: "94%",
+    top: "30%",
+  },
+
+  botonPortada: {
+    paddingTop: 20,
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 10,
+    height: 50,
+    width: "75%",
+    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+
+  textoBoton: {
+    color: "white",
+    bottom: 11
+  },
+
+  iconImportarClave: {
+    bottom: 10,
+    right: 30,
+    color: 'white',
+  },
+});
+
+
+export default WelcomeScreen;
