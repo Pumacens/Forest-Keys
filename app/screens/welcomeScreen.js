c91dc2968ed6292b8c5979447eb5dbe7df973d92
pumacens <dafflorezc@correo.udistrital.edu.co>
2020-10-21 22:58:35 -0500

    Added metro config file, added chinook database to test expo Sqlite, and it worked finally :'''''')

--- app/screens/welcomeScreen.js
+++ app/screens/welcomeScreen.js
@@ -9,35 +9,64 @@ import {
 } from "react-native";
 import { loadAsync } from "expo-font";
 import { LinearGradient } from "expo-linear-gradient";
-import * as SQLite from 'expo-sqlite';
-import * as FileSystem from 'expo-file-system';
-import * as MediaLibrary from 'expo-media-library';
-import * as Permissions from 'expo-permissions';
-import { AppLoading } from 'expo';
+import * as SQLite from "expo-sqlite";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
+import * as Permissions from "expo-permissions";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
 
 import TopMenu from "../components/topMenu";
 import KeysDropdown from "../components/keysDropdown";
 import data from "../config/deviceData";
 
-const db = SQLite.openDatabase('chinook.db');
+const db = SQLite.openDatabase("chinook.db");
 
 class WelcomeScreen extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dbDir: `${FileSystem.documentDirectory}SQLite/chinook.db`,
+    };
+
     this.state.isReady = false;
-    
   }
 
   loadAssetsAsync = async () => {
-    // Load a font `Montserrat` from a static resource
-    this.state.backgroundImage = require("../assets/portada2-op.png"),
+    // Load a font SedanSC and Background image
+    (this.state.backgroundImage = require("../assets/portada2-op.png")),
+      await loadAsync({
+        SedanSC: require("../assets/fonts/SedanSC-Regular.ttf"),
+      });
 
-    await loadAsync({
-      SedanSC: require('../assets/fonts/SedanSC-Regular.ttf'),
-    }) 
-  }
+    // Create dummy db to make SQlite folder
+    const dbTest = SQLite.openDatabase("dummy.db");
+
+    try {
+      await dbTest.transaction((tx) => tx.executeSql(""));
+      // console.log("Se creó directorio de base de datos: ", dbTest);
+      FileSystem.deleteAsync(`${FileSystem.documentDirectory}SQLite/dummy.db`);
+    } catch (e) {
+      console.log("error while executing SQL in dummy DB: ", e);
+    }
+
+    //Import db chinook from assets
+
+    let dbAssetdir = Asset.fromModule(require("../assets/db/chinook.db")).uri;
+
+    try{
+      await FileSystem.downloadAsync(dbAssetdir, this.state.dbDir)
+      this.db = SQLite.openDatabase("chinook.db");
+      // console.log(db);
+
+    } catch(e){
+      console.log("Excepcion reading chinook.db");
+      console.log(ex);
+      FileSystem.deleteAsync(this.state.dbDir);
+    }
+  };
+
 
   handleChangeToKeyScreen = async () => {
     // db.transaction(tx => {
@@ -48,24 +77,19 @@ class WelcomeScreen extends Component {
     //   );
     // });
 
-    let dir = await FileSystem.readDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
-    let docsList = [];
-
-    dir.forEach((val) => {
-      docsList.push(FileSystem.documentDirectory + 'SQLite/' + val);
+    this.db.transaction((tx) => {
+      tx.executeSql("select * from media_types", [], (trans, result) => { 
+        console.log(".......................................................");
+        result.rows._array.map(item => console.log(item));
+      });
     });
-
-    console.log(docsList);
   };
-  
 
   handleChangeToSpeciesScreen = () => {
     this.props.navigation.navigate("SpeciesListScreen");
   };
 
-
-  render(){ 
-
+  render() {
     if (!this.state.isReady) {
       return (
         <AppLoading
@@ -75,68 +99,71 @@ class WelcomeScreen extends Component {
         />
       );
     }
-    
-    return (
-    <ImageBackground
-      style={styles.portada}
-      source={this.state.backgroundImage}
-    >
-      <LinearGradient
-        colors={["rgba(0,0,0,0.85)", "rgba(0,0,0,0.20)", "rgba(0,0,0,1)"]}
-        locations={[0.5, 0.8, 1]}
-      >
-        <View style={styles.hero}>
-          <TopMenu style={styles.botonMenuSuperior} />
-          <Text
-            style={[
-              styles.texto,
-              styles.titulo,
-              {
-                fontFamily: 'SedanSC',
-              },
-            ]}
-          >
-            ForestKeys
-          </Text>
-          <Text style={[styles.subTitulo]}>Seleccione la clave a utilizar</Text>
-          <KeysDropdown styles={styles.menuClaves} />
-        </View>
 
-        <View style={styles.seccionPortadaBotones}>
-          <TouchableNativeFeedback
-            onPress={this.handleChangeToKeyScreen}
-            title="IDENTIFICAR PLANTA"
-          >
-            <View style={styles.botonPortada}>
-              <Text style={styles.textoBoton}>IDENTIFICAR PLANTA</Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback
-            title="LISTA DE ESPECIES DE LA CLAVE"
-            onPress={() => this.handleChangeToSpeciesScreen(this.props)}
-          >
-            <View style={styles.botonPortada}>
-              <Text style={styles.textoBoton}>
-                LISTA DE ESPECIES DE LA CLAVE
-              </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback title="IMPORTAR CLAVE">
-            <View style={styles.botonPortada}>
-              <FontAwesome5
-                size={20}
-                style={styles.iconImportarClave}
-                name="file-import"
-              />
-              <Text style={[styles.textoBoton, { right: 7 }]}>
-                IMPORTAR CLAVE
-              </Text>
-            </View>
-          </TouchableNativeFeedback>
-        </View>
-      </LinearGradient>
-    </ImageBackground>
-  )};
+    return (
+      <ImageBackground
+        style={styles.portada}
+        source={this.state.backgroundImage}
+      >
+        <LinearGradient
+          colors={["rgba(0,0,0,0.85)", "rgba(0,0,0,0.20)", "rgba(0,0,0,1)"]}
+          locations={[0.5, 0.8, 1]}
+        >
+          <View style={styles.hero}>
+            <TopMenu style={styles.botonMenuSuperior} />
+            <Text
+              style={[
+                styles.texto,
+                styles.titulo,
+                {
+                  fontFamily: "SedanSC",
+                },
+              ]}
+            >
+              ForestKeys
+            </Text>
+            <Text style={[styles.subTitulo]}>
+              Seleccione la clave a utilizar
+            </Text>
+            <KeysDropdown styles={styles.menuClaves} />
+          </View>
+
+          <View style={styles.seccionPortadaBotones}>
+            <TouchableNativeFeedback
+              onPress={this.handleChangeToKeyScreen}
+              title="IDENTIFICAR PLANTA"
+            >
+              <View style={styles.botonPortada}>
+                <Text style={styles.textoBoton}>IDENTIFICAR PLANTA</Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback
+              title="LISTA DE ESPECIES DE LA CLAVE"
+              onPress={() => this.handleChangeToSpeciesScreen(this.props)}
+            >
+              <View style={styles.botonPortada}>
+                <Text style={styles.textoBoton}>
+                  LISTA DE ESPECIES DE LA CLAVE
+                </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback title="IMPORTAR CLAVE">
+              <View style={styles.botonPortada}>
+                <FontAwesome5
+                  size={20}
+                  style={styles.iconImportarClave}
+                  name="file-import"
+                />
+                <Text style={[styles.textoBoton, { right: 7 }]}>
+                  IMPORTAR CLAVE
+                </Text>
+              </View>
+            </TouchableNativeFeedback>
+          </View>
+        </LinearGradient>
+      </ImageBackground>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
